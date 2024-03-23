0f7ee320229cb055b7af045bab40ef3945b9cbfa
Dmoreno2903 <jaguirremo@unal.edu.co>
2024-03-23 13:11:14 -0500

    Plus: Django add

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Crustaceo-Cascarudo
-Proyecto de la materia Desarrollo web - 2024
+# Crustaceo cascarudo
+Página web del restaurante de Don Cangrejo
+
+## Descripción
+Este proyecto es una página web del restaurante de Don Cangrejo, el Crustáceo Cascarudo. En ella se pueden encontrar los platos que se sirven además de poder realizar pedidos.
+
+## Tecnologías
+### Frontend
+### Backend
+- El backend está desarrollado usando el framework `Django` de `Python`
+- Se usa `django-rest-framework` para la creación de una API REST.
+### Base de datos
+- La base de datos usada es `PostgreSQL`
---
+++ backend/configuration/settings.py
@@ -0,0 +1,106 @@
+# libraries
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+# Path of the project
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Environment variables
+load_dotenv(f'../{BASE_DIR}/.env')
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+DEBUG = os.getenv('DEBUG')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
+
+# Installed apps
+BASE_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+LOCAL_APPS = []
+
+THIRD_APPS = []
+
+INSTALLED_APPS = BASE_APPS + LOCAL_APPS + THIRD_APPS
+
+# Middleware
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# Root URL
+ROOT_URLCONF = 'configuration.urls'
+
+# Template
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+# WSGI
+WSGI_APPLICATION = 'configuration.wsgi.application'
+
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
+    }
+}
+
+# Password validation
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+# Internationalization
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+# Static and media files
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+# Default auto field
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
