44e4b62be81b4d8f2acf92c6bb01060681a16a2b
sebstianzamora <69611081+sebstianzamora@users.noreply.github.com>
2024-07-07 17:15:38 -0500

    Frontend Inicio de sesión y registro de usuario

--- frontend/src/pages/InicioDeSesion.jsx
+++ frontend/src/pages/InicioDeSesion.jsx
@@ -1,6 +1,53 @@
+import {useForm} from "react-hook-form";
+import {Link} from "react-router-dom";
+import '../styles/pages/InicioDeSesion.css'
 
 export const InicioDeSesion = () => {
-  return (
-    <div>InicioDeSesion</div>
-  )
+
+    
+    const {register, handleSubmit} = useForm()
+
+    const onSubmit = handleSubmit((data) =>{
+        console.log(data);
+    })
+
+    return (
+        <div className="form-container">
+          
+          <div>
+            
+          <h1 className="titulo">Iniciar sesión</h1>
+
+          <form onSubmit={handleSubmit((values) => console.log(values))}>
+            
+            <div className="form-group">
+              <label htmlFor="username">Nombre de usuario</label>
+              <input
+                type="text"
+                {...register("username", { required: true })}
+                placeholder="Nombre de usuario"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Contraseña</label>
+              <input
+                type="password"
+                {...register("password", { required: true })}
+                placeholder="Contraseña"
+              />
+            </div>
+            
+            <button type="submit" className="submit-button">
+              Ingresar
+            </button>
+          </form>
+          </div>
+          <div>
+            <h2>¿No tienes cuenta?</h2>
+            <Link className="link" to="/registro">
+            Regístrate
+            </Link>
+            </div>
+        </div>
+      );
 }
--- frontend/src/pages/Registro.jsx
+++ frontend/src/pages/Registro.jsx
@@ -1,6 +1,88 @@
+import {useForm} from "react-hook-form"
+import {Link} from "react-router-dom";
+import '../styles/pages/Registro.css'
 
 export const Registro = () => {
-  return (
-    <div>Registro</div>
-  )
+
+    const {register, handleSubmit} = useForm()
+
+    return (
+        <div className="inicio-form-container">
+          
+          <div>
+            
+          <h1 className="inicio-titulo">Regístrate</h1>
+
+          <form onSubmit={handleSubmit((values) => console.log(values))}>
+            <div className="inicio-form-group">
+              <label htmlFor="firstName">Nombres</label>
+              <input
+                type="text"
+                {...register("firstName", { required: true })}
+                placeholder="Nombres"
+              />
+            </div>
+            <div className="inicio-form-group">
+              <label htmlFor="lastName">Apellidos</label>
+              <input
+                type="text"
+                {...register("lastName", { required: true })}
+                placeholder="Apellidos"
+              />
+            </div>
+            <div className="inicio-form-group">
+              <label htmlFor="email">Correo electrónico</label>
+              <input
+                type="email"
+                {...register("email", { required: true })}
+                placeholder="Correo electrónico"
+              />
+            </div>
+            <div className="inicio-form-group">
+              <label htmlFor="username">Nombre de usuario</label>
+              <input
+                type="text"
+                {...register("username", { required: true })}
+                placeholder="Nombre de usuario"
+              />
+            </div>
+            <div className="inicio-form-group">
+              <label htmlFor="password">Contraseña</label>
+              <input
+                type="password"
+                {...register("password", { required: true })}
+                placeholder="Contraseña"
+              />
+            </div>
+            <div className="inicio-form-group">
+              <label htmlFor="confirmPassword">Confirmar contraseña</label>
+              <input
+                type="password"
+                {...register("confirmPassword", { required: true })}
+                placeholder="Confirmar contraseña"
+              />
+            </div>
+            <div className="inicio-form-group inicio-checkbox-label">
+              <input
+                type="checkbox"
+                {...register("terms", { required: true })}
+                id="terms"
+              />
+              <label htmlFor="terms">Acepto las políticas de tratamiento de datos</label>
+            </div>
+            <button type="submit" className="submit-button">
+              Registrarse
+            </button>
+          </form>
+          </div>
+          <div>
+            <h2>¿Ya tienes cuenta?</h2>
+            <Link className="inicio-link" to="/inicio-de-sesion">
+            Iniciar sesión
+            </Link>
+            </div>
+        </div>
+      );
 }
+
+
