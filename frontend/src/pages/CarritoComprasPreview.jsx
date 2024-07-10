ee24bb49fc1a97b8b5ff590a56f1bda144883989
soandrade6 <sofia.andradep06@gmail.com>
2024-07-09 21:01:51 -0500

    Tabla de carrito de compras

--- frontend/src/pages/CarritoComprasPreview.jsx
+++ frontend/src/pages/CarritoComprasPreview.jsx
@@ -1,7 +1,30 @@
+import { useContext } from "react"
+import { CarritoContext } from "../context/CarritoContext"
+import "../styles/pages/CarritoComprasPreview.css";
 
 export const CarritoComprasPreview = () => {
+    
   return (
-    <div>CarritoComprasPreview</div>
+    <>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th scope="col"></th>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Precio</th>
+                        <th scope="col">Cantidad</th>
+                        <th scope="col"></th>
+                    </tr>
+                </thead>
+               
+            </table>
+            <div className="d-flex justify-content-center">
+                <button 
+                className="btn btn-primary w-25"
+                >Comprar</button>
+            </div>
+            
+        </>
   )
 }
 
