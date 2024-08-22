e93dc615c599603d2a8e2145db84ae748a81cb27
Sergio Arias <s-andres140@hotmail.com>
2024-08-22 15:53:17 -0500

    add creacion de datos estaticos de usuario

---
+++ frontend/src/dataMomentanea/users.js
@@ -0,0 +1,68 @@
+export const CLIENT = [{
+        id:1,
+        type: "Client",
+        name: 'Diego Moreno',
+        username: 'dmoreno',
+        email: 'jaguirremo@unal.edu.co',
+        age: 22,
+        city: 'Medellín',
+        country: 'Colombia',
+        occupation: 'Ingeniero de sistemas',
+        address: 'calle 8',
+        phone: '+57 3017549300',
+        accountType: 'ahorro', //solo ahorro o corriente
+        accountNumber: 74,
+        profilePicture: 'https://via.placeholder.com/150',
+      },
+      {
+        id:2,
+        type: "Client",
+        name: 'Sofi',
+        username: 'sofi23',
+        email: 'sofi@unal.edu.co',
+        age: 22,
+        city: 'Medellín',
+        country: 'Colombia',
+        occupation: 'Ingeniero de sistemas',
+        address: 'calle 9',
+        phone: '+57 3017549300',
+        accountType: 'corriente', //solo ahorro o corriente
+        accountNumber: 84,
+        profilePicture: 'https://via.placeholder.com/150',
+      },
+      {
+        id: 3,
+        type: "Client",
+        name: 'Sebas',
+        username: 'sebas23',
+        email: 'sebas@unal.edu.co',
+        age: 22,
+        city: 'Medellín',
+        country: 'Colombia',
+        occupation: 'Ingeniero de sistemas',
+        address: 'calle 10',
+        phone: '+57 3017549300',
+        accountType: 'corriente', //solo ahorro o corriente
+        accountNumber: 85,
+        profilePicture: 'https://via.placeholder.com/150',
+      },
+]
+
+export const Administrator = [
+    {
+        id:1,
+        type: "Administrator",
+        name: 'Sergio',
+        username: 'sarias',
+        email: 'sarias@unal.edu.co',
+        age: 26,
+        city: 'Medellín',
+        country: 'Colombia',
+        occupation: 'Ingeniero de sistemas',
+        address: 'calle 11',
+        phone: '+57 3017549300',
+        accountType: 'corriente', //solo ahorro o corriente
+        accountNumber: 95,
+        profilePicture: 'https://via.placeholder.com/150',
+      }
+]
