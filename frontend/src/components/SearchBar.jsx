b79ebb294e4357c85f71f7660ff88afd022237ce
Sergio Arias <s-andres140@hotmail.com>
2024-08-22 14:27:52 -0500

    add funcionalidad barra de busqueda

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,9 +1,62 @@
+import { useContext, useState } from "react";
+import "../styles/SearchBar.css";
+import { AuthContext } from "../context/AuthContextProvider";
+import { useNavigate } from "react-router-dom";
 
 const SearchBar = () => {
 
+  const { burguers, fries, drinks } = useContext(AuthContext)
+
+  const navigate = useNavigate()
+
+  const items = [
+      ...burguers,
+      ...fries,
+      ...drinks
+  ]
+
+  const [searchTerm, setSearchTerm] =useState('')
+  const [filteredItems, setFilteredItems] = useState([])
+
+  const handleSearch = (event) => {
+    const term = event.target.value.toLowerCase();
+    setSearchTerm(term);
+
+    if (term === '') {
+      setFilteredItems([]) // Si no hay término de búsqueda, no muestra nada
+    } else {
+      const filtered = items.filter(item => 
+        item.name.toLowerCase().includes(term)
+      ).slice(0, 5)
+
+      setFilteredItems(filtered)
+    }
+  }
+  console.log("filtered",filteredItems)
+
+  const handleInfoClick = (table, id) => {
+    navigate(`/detallado/${table}/${id}`)
+    setSearchTerm('')
+    setFilteredItems([])
+  }
+
   return (
-    <div>
-        <input type='text' placeholder='Buscar productos...' className='barra-busqueda'/>
+    <div className="search-container">
+        <input
+          type='text'
+          placeholder='Buscar productos...'
+          className='barra-busqueda'
+          value={searchTerm}
+          //El evento onChange es un manejador de eventos que se activa cada vez que el valor de un elemento de entrada (como un input, textarea, o select) cambia.
+          onChange={handleSearch}
+        />
+        {filteredItems.length > 0 && (
+                <ul className="search-results">
+                    {filteredItems.map((item, index) => (
+                        <li key={index} onClick={()=>handleInfoClick(item.table, item.id)}>{item.name}</li>
+                    ))}
+                </ul>
+        )}
     </div>
   )
 }
